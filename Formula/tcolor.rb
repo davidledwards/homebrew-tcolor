28393ca3988c303543c456fc674724a73e698712
David Edwards <david.l.edwards@gmail.com>
2025-02-04 21:50:11 -0600

    prepare for 0.2.1 release

--- Formula/tcolor.rb
+++ Formula/tcolor.rb
@@ -1,15 +1,15 @@
 class Tcolor < Formula
   desc "Displays ANSI colors on the terminal"
   homepage "https://github.com/davidledwards/tcolor"
-  version "0.2.0"
+  version "0.2.1"
 
   if OS.linux? || OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/davidledwards/tcolor/releases/download/v" + version + "/tcolor-" + version + "-x86_64-unix.tar.gz"
-      sha256 "0dec4bb6996095152d614e016cb4de464a7ce0be53f8f30373b83ffd24ab6c68"
+      sha256 "da61fd771fba652d6dcbd8c093163e1e25dd041797d7c8f3b9c8f3936a9e9300"
     elsif Hardware::CPU::arm?
       url "https://github.com/davidledwards/tcolor/releases/download/v" + version + "/tcolor-" + version + "-aarch64-unix.tar.gz"
-      sha256 "867c211540ed407cc5c6c3a7c9ad2f96d85180b08f498f2e27ec5da1cf2308b7"
+      sha256 "254c5cc39b5e7e281fe29e88e45b5bc470efc8a8b3ae9855aeb0fe6f5b6e739a"
     end
   end
 
